333102ed5b9a72af4204193d06a2f6e11000db74
Mandaryn <mandaryyyn@gmail.com>
2016-09-24 23:37:34 +0300

    build middleman with travis

--- config.rb
+++ config.rb
@@ -77,6 +77,15 @@ configure :build do
   activate :deploy do |deploy|
     deploy.deploy_method = :git
     deploy.strategy = :submodule
+
+    committer_app = "#{Middleman::Deploy::PACKAGE} v#{Middleman::Deploy::VERSION}"
+    commit_message = "Deployed using #{committer_app}"
+
+    if ENV["TRAVIS_BUILD_NUMBER"] then
+      commit_message += " (Travis Build \##{ENV["TRAVIS_BUILD_NUMBER"]})"
+    end
+
+    deploy.commit_message = commit_message
   end
 end
 
