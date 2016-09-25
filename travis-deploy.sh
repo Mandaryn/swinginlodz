3b4a9b8627689c28f80b8b1fc874a2e8b06ebde1
Mandaryn <mandaryyyn@gmail.com>
2016-09-25 03:03:46 +0300

    travis deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,9 +7,7 @@ fi
 
 echo -e "\nRunning Travis Deployment"
 echo "Setting up Git Access"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+git remote set-url origin "https://${GH_TOKEN}:@github.com/Mandaryn/swinginlodz.git"
 git config --global user.name ${GH_COMMIT_AUTHOR}
 git config --global user.email ${GH_COMMIT_EMAIL}
-
 bundle exec rake deploy
